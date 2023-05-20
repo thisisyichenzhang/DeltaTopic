89dc554cac7cd264a10d54912817f7ee8fd6b5fb
thisisyichenzhang <zhangyichen93@gmail.com>
2023-05-20 12:43:17 -0700

    toy datasets

---
+++ DeltaTopic/datasets/_datasets.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from typing import Optional, Union
+from scanpy import read
+
+url_datadir = "https://github.com/causalpathlab/DeltaTopic/tree/benchmark_dev/"
+
+def toy_data(file_path: Union[str, Path] = "data/test.h5ad"):
+    """toy_example.
+
+    Arguments
+    ---------
+    file_path
+        Path where to save dataset and read it from.
+
+    Returns
+    -------
+    Returns `adata` object
+    """
+    url = f"{url_datadir}data/toy_data.h5ad"
+    adata = read(file_path, backup_url=url, sparse=True, cache=True)
+    #adata.var_names_make_unique()
+    return adata
+
+
+
